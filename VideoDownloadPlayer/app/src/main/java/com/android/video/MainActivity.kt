27fde6e471bcaa73c98a2c62de54cb2895171033
GL <gl@GaoLeideMacBook-Pro.local>
2019-06-20 07:25:29 +0800

    添加视频链接

--- VideoDownloadPlayer/app/src/main/java/com/android/video/MainActivity.kt
+++ VideoDownloadPlayer/app/src/main/java/com/android/video/MainActivity.kt
@@ -8,5 +8,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val videos: Array<String> = resources.getStringArray(R.array.videos);
+        val videosName: Array<String> = resources.getStringArray(R.array.videos_name);
+        val images: List<Int> = mutableListOf(
+            R.mipmap.img1,
+            R.mipmap.img2,
+            R.mipmap.img3,
+            R.mipmap.img4,
+            R.mipmap.img5,
+            R.mipmap.img6,
+            R.mipmap.img7,
+            R.mipmap.img8,
+            R.mipmap.img9,
+            R.mipmap.img10
+        )
+
+
     }
 }
