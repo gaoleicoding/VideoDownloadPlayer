c4ac6e24e0d035416ad69203f01a2c9fa91c3f8a
GL <gl@GaoLeideMacBook-Pro.local>
2019-06-24 07:19:18 +0800

    3

--- VideoDownloadPlayer/app/src/main/java/com/android/video/MainActivity.kt
+++ VideoDownloadPlayer/app/src/main/java/com/android/video/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
---
+++ VideoDownloadPlayer/app/src/main/java/com/android/video/download/DownloadManager.kt
@@ -0,0 +1,5 @@
+package com.android.video.download
+
+class DownloadManager {
+
+}
